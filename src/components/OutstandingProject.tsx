600b4bd94335370c74746950bb5545ca8f506ff7
ningyu <ningyu@estl.sg>
2023-04-07 21:31:35 +0800

    update pages

--- src/components/OutstandingProject.tsx
+++ src/components/OutstandingProject.tsx
@@ -1,14 +1,14 @@
 import { Card } from "react-bootstrap"
 import { Assignment } from "./Assignment"
 
-export const Project = ({ data }: any) => {
+export const OutstandingProject = ({ data }: any) => {
   const scriptCount = data.assignments.reduce((count: number, a: any) => a.scripts.length + count, 0)
   const assignmentCount = data.assignments.length;
 
   return (
     <Card>
       <Card.Body>
-        <h1>{data.name}</h1>
+        <h1>{data.name} | {data.period}</h1>
         <div>{data.id} | You have {scriptCount} outstanding scripts in {assignmentCount} assignments.</div>
         {data.assignments.map((a: any) => <Assignment data={a} key={a.id} />)}
       </Card.Body>
--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,18 +1,17 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
+import { Button } from 'react-bootstrap'
 import { useRouter } from 'next/router'
 import { AppNavBar } from '@/components/UserNavBar'
 import { fetchGet } from '@/lib/fetch'
 import { useEffect, useState } from 'react'
 import { OutstandingProject } from '@/components/OutstandingProject'
 
-export default function Home() {
-  const router = useRouter();
-
+export default function Projects() {
   const [data, setData] = useState<any>([]);
 
   useEffect(() => {
-    fetchGet('/api/outstanding').then(data => {
+    fetchGet('/api/myprojects').then(data => {
       setData(data);
     })
   }, [])
